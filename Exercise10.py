9a56d5aad2558e1dad53aa6df90423c3b0fce49d
Rmetro <reubengabriel2006@gmail.com>
2024-11-09 22:51:36 +0400

    Create Exercise10.py
    Checks if a number is even or odd and prints the result.

---
+++ Exercise10.py
@@ -0,0 +1,18 @@
+#check if number is even or odd
+def check_even_or_odd(number):
+    if number % 2 == 0:
+         return f"{number} is even."
+    else:
+        return f"{number} is odd."
+def main():
+    # Ask user for a number
+    number = int(input("Enter a number:"))
+    
+    #number to the function and print the result
+    result = check_even_or_odd(number)
+    print(result)
+
+#maKe sure the main function works
+if __name__ == "__main__":
+     main()
+         
